fc39da54f32e6740994d55ff8a3d374af2360a0d
Chris Vasconi <vasconichris@gmail.com>
2022-06-06 14:11:39 -0400

    loopsbasic

---
+++ forloopsbasic.py
@@ -0,0 +1,35 @@
+for x in range(0, 151):
+    print(x)
+
+for i in range(1, 151):
+    if(i % 5 == 0):
+        print(i)
+
+
+for i in range(1, 100):
+    if(i % 5 == 0):
+        print("coding")
+    else:
+        (i % 10 == 0)
+    print("Coding Dojo")
+    else:
+    print(i)
+
+sum = 0
+
+for i in range(0, 500000):
+    for (i % 2 !- 0):
+    sum += i
+print(sum)
+
+for x in range(2018, 0, -4):
+    print(x)
+
+lowNum = 2
+highNum = 9
+mult = 3
+
+for x in range(lowNum, highNum+1):
+
+    if(x % mult == 0):
+        print(x)
